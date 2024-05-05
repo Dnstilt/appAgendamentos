d80ea0b8437284d6ea6085b99bb6f15b441fb4aa
Dnstilt <denistakenaka@gmail.com>
2024-05-05 20:52:12 -0300

    politica de privacidade

---
+++ js/components/footerPrincipal.js
@@ -0,0 +1,18 @@
+document.getElementById('footerPrincipal').innerHTML = ` 
+<div class="mt-5" style="background-color: #a30029;">
+    <div class="container" style=" width: 100%;">
+        <div class="row p-3">
+        <div class="col-sm-12 col-md-6 " class="text-light">
+            <p class="text-light">2024 Urutau.inc - Todos os direitos reservados</p>
+              
+            <p class="text-light">Rua Laurindo José de Souza, 1150 Barra da lagoa - florianópolis SC</p>
+        </div>
+        <div class="col-sm-12 col-md-6 text-end">
+            <a href="./politica_de_privacidade.html" class="text-decoration-none text-light">Política de privacidade</a>
+            <a href="./termos_de_uso" class="text-decoration-none text-light">Termos de uso</a>
+            <p class="text-light">Imagens from freepik</p>
+        </div>
+        </div>    
+    </div>
+</div>
+`;
